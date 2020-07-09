6e2331da9a86307ec44ab4cd6212da1c17a2613b
Jonathan <Jonnyghan@gmail.com>
2020-07-09 00:05:18 -0400

    handled the routing and set up my .env with my FB oAuth tokens

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,7 +5,7 @@ class PlayersController < ApplicationController
   def create
   end
 
-  def show
+  def index
   end
 
   def edit
@@ -14,6 +14,7 @@ class PlayersController < ApplicationController
   def update
   end
 
-  def destroyy
+  def destroy
+  
   end
 end
